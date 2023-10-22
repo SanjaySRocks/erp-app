43569dc942ab2a16d716eb409fa7a8feb05814c9
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2023-10-22 14:18:53 +0530

    create login page

---
+++ src/app/faculty/login/page.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import FacultyLoginCard from '@/app/components/faculty-login-card'
+
+const FacultyLogin = () => {
+  return (
+    <>
+      <div className="container pt-5">
+
+        <div className="row justify-content-center">
+
+          <div className="col-6 col-md-6 ">
+
+            <FacultyLoginCard/>
+            
+          </div>
+
+
+        </div>
+
+
+      </div>
+    </>
+  )
+}
+
+export default FacultyLogin
