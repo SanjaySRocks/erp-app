7348c59bcb66ff7fb11e2ff4b45830d7a9a42e7e
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2023-10-22 14:19:13 +0530

    create login page

---
+++ src/app/student/login/page.js
@@ -0,0 +1,25 @@
+import StudentLoginCard from "@/app/components/student-login-card"
+
+const LoginPage = () => {
+  return (
+    <>
+      <div className="container">
+
+        <div className="row justify-content-center">
+
+          <div className="col-6 col-md-6 ">
+
+            <StudentLoginCard/>
+
+          </div>
+
+
+        </div>
+
+
+      </div>
+    </>
+  )
+}
+
+export default LoginPage
